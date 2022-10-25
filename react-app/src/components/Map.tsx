1f2a65602f3f8a3b3c40a6f238bc57ab6b50e34c
Ji Wang <wj42421999@gmail.com>
2022-10-25 16:36:06 -0400

    Resize the embedded Shiny app

--- react-app/src/components/Map.tsx
+++ react-app/src/components/Map.tsx
@@ -1,8 +1,18 @@
+import { Col, Row } from "antd";
+
 function Map() {
+//    const url = "https://theglacierproject.shinyapps.io/GlacierProject/"; // Production use
+    const PORT = 5671; // Dev use. Specific the PORT of a local Shiny App when developing
+    const url = `http://localhost:${PORT}`;
     return (
-        <object data="https://theglacierproject.shinyapps.io/GlacierProject/"
-                type="text/html" id="appObject">
-        </object>
+        <Row justify="center" align="middle" style={{height: "100vh"}}>
+            <Col span={24} style={{height: "100vh", width: "100vw"}}>
+
+                <object data={url} type="text/html" id="appObject" style={{height: "100vh", width: "100vw"}}>
+                    Alternate Content
+                </object>
+            </Col>
+        </Row>
     );
 }
 
