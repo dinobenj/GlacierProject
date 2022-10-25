a5527ff0ae1af6d40dc6a78734d4d8382ea0117e
ryankarch <karchryan@gmail.com>
2022-10-25 17:34:49 -0400

    Initial recursive logic for get_regions

--- backend/img-algos/img.py
+++ backend/img-algos/img.py
@@ -3,28 +3,28 @@ import PIL
 
 def determine_color(r: int, g: int, b: int) -> str:
 
-    if g > r and g > b:
-        pass
-
-
-
     pass
 
 
 def traverse_glacier_image(imgfile):
 
     regions = { 
-        "green": [],
-        "yellow": [],
-        "orange": [],
-        "white": []
+        "green": [[]],
+        "yellow": [[]],
+        "orange": [[]],
+        "white": [[]]
     }
+    # do funky logic to add new region to new list within list in dict
+    def get_regions(pixels: list, last_pixel: tuple, next_pixel: tuple, region: int):
+        
+        if determine_color(*last_pixel) != determine_color(*next_pixel):
+            # change region num to reflect num
+            get_regions(regions[determine_color(*next_pixel)], next_pixel, region+1)
 
-    def get_region(pixels: list):
-
+            return
         pass
 
-
+    # get_regions(re)
     return regions
 
 
