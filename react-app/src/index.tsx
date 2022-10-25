eddeb20c28393cdede69307e3f768c99f60ed579
Ji Wang <wj42421999@gmail.com>
2022-10-24 22:29:30 -0400

    Set routing

--- react-app/src/index.tsx
+++ react-app/src/index.tsx
@@ -1,16 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import Home from './components/Home';
+import Map from "./components/Map";
+import SiteHeader from './components/SiteHeader';
+import SiteFooter from './components/SiteFooter';
+import backgroundImage from './assets/background.jpg'
+import 'antd/dist/antd.min.css';
+import {Layout} from 'antd';
+
+const {Content} = Layout;
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Home/>
+    },
+    {
+        path: "/map",
+        element: <Map/>
+    }
+]);
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+    document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <Layout style={{height: "100vh"}}>
+            <SiteHeader/>
+            <Content style={{backgroundImage: `url${backgroundImage}`}}>
+                <RouterProvider router={router}/>
+            </Content>
+            <SiteFooter/>
+        </Layout>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
