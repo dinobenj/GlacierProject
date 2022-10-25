9815e5cb75466a36afe59d258059ea0619d0b533
Ji Wang <wj42421999@gmail.com>
2022-10-25 17:44:17 -0400

    Wrap homepage's intro and button with Typography

--- react-app/src/components/Home.tsx
+++ react-app/src/components/Home.tsx
@@ -1,19 +1,31 @@
 import React from "react";
-import {Button, Col, Row} from "antd";
+import {Button, Col, Row, Typography} from "antd";
+
+const {Title, Paragraph, Text} = Typography;
 
 const Home: React.FC = () => {
+    const introduction = "Glaciers are masses of snow that has been compressed into giant sheets of ice. Most glaciers were formed during the last ice age.";
+
     return (
         <Row justify="center" align="middle" style={{height: "100vh"}}>
-            <Col>
+            <Col span={12} style={{
+                padding: "20px",
+                borderRadius: "20px",
+                backgroundColor: "rgba(128, 128, 128, 0.3)"
+            }}>
                 <Row justify="space-around" align="middle">
-                    <Col>
-                        <h1>Explore Glaciers</h1>
+                    <Col span={24}>
+                        <Paragraph strong style={{color: "white"}}>
+                            {introduction}
+                        </Paragraph>
                     </Col>
                 </Row>
                 <Row justify="space-around" align="middle">
                     <Col>
                         <Button type="text" size="large" shape="round" href="/map">
-                            Go to map
+                            <Text strong style={{color: "white"}}>
+                                Explore in map
+                            </Text>
                         </Button>
                     </Col>
                 </Row>
