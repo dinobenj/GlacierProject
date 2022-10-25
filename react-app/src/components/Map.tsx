ef8639a7a032feffc577eb7cb5e665721431aeb7
Ji Wang <wj42421999@gmail.com>
2022-10-24 22:31:01 -0400

    Set map page (the embedded Shiny App)

---
+++ react-app/src/components/Map.tsx
@@ -0,0 +1,9 @@
+function Map() {
+    return (
+        <object data="https://theglacierproject.shinyapps.io/GlacierProject/"
+                type="text/html" id="appObject">
+        </object>
+    );
+}
+
+export default Map;
