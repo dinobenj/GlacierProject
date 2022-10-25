c0420996dc5c7c802ee09eab43273a33cc144da0
ryankarch <karchryan@gmail.com>
2022-10-25 16:24:55 -0400

    img-algo structure

---
+++ backend/img-algos/img.py
@@ -0,0 +1,19 @@
+import PIL
+
+
+def traverse_glacier_image(imgfile):
+
+    types = { 
+        "glacier": [],
+        "river": [],
+        "...": []
+    }
+
+    def glacier_pixels(pixels: list):
+
+        pass
+
+    def river_pixels(pixels: list):
+        pass
+
+    return types
