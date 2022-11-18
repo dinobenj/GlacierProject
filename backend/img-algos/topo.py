5ef4993e6dbdd13be7a566bc32f2bddfd4dd66ba
ryankarch <karchryan@gmail.com>
2022-11-18 16:24:53 -0500

    Elevation data request

--- backend/img-algos/topo.py
+++ backend/img-algos/topo.py
@@ -3,7 +3,11 @@ import urllib3
 import urllib
 import pandas as pd
 import numpy as np
-import elevation
+# import elevation
+
+import geopandas as gpd
+import rasterio
+
 
 GLACIER_DATA = pd.read_csv("test.csv", usecols=["NAME", "LATITUDE", "LONGITUDE"], encoding="latin1")
 
@@ -15,16 +19,38 @@ def get_lat_long(name: str) -> tuple:
     gd = GLACIER_DATA.loc[GLACIER_DATA["NAME"] == name]
     return gd.iloc[0]["LATITUDE"], gd.iloc[0]["LONGITUDE"]
 
+def make_elevation_request(lat, long):
+    
+
+    headers = {
+        'referer': 'https://www.freemaptools.com/elevation-finder.htm',
+    }
+
+    params = (
+        ('v', '4'),
+        ('lat', str(lat)),
+        ('lng', str(long)),
+    )
+
+    response = requests.get('https://www.freemaptools.com/ajax/elevation-service.php', headers=headers, params=params)
+    return response.text
+#NB. Original query string below. It seems impossible to parse and
+#reproduce query strings 100% accurately so the one below is given
+#in case the reproduced version is not "correct".
+# response = requests.get('https://www.freemaptools.com/ajax/elevation-service.php?v=4&lat=-64.00000&lng=-65.00000', headers=headers)
+
+
 if __name__ == "__main__":
     # print(GLACIER_DATA)
     thunder = get_lat_long("THUNDER")
     print(thunder)
 
+    print(make_elevation_request(thunder[0], thunder[1]))
     # for col in GLACIER_DATA.columns:
     #     print(len(col))
-    elevation.clip(bounds=(thunder[0] - 0.1, thunder[1] - 0.1, thunder[0] + 0.1, thunder[1] + 0.1), output='test.tif')
+    # elevation.clip(bounds=(thunder[0] - 0.1, thunder[1] - 0.1, thunder[0] + 0.1, thunder[1] + 0.1), output='test.tif')
 
-    elevation.clean()
+    # elevation.clean()
     # elevation_function(get_lat_long("THUNDER"))
     # print(get_lat_long("NO 4"))
 
