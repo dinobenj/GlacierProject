36672ef38464d373f2cf1a7f61297a6b7d790133
ryankarch <karchryan@gmail.com>
2022-12-08 15:05:16 -0500

    file cleanup

--- backend/img-algos/topo.py
+++ backend/img-algos/topo.py
@@ -3,16 +3,11 @@ import urllib3
 import urllib
 import pandas as pd
 import numpy as np
-# import elevation
-
 import geopandas as gpd
 import rasterio
 
 
 GLACIER_DATA = pd.read_csv("test.csv", usecols=["NAME", "LATITUDE", "LONGITUDE"], encoding="latin1")
-
-# GLACIER_DATA = GLACIER_DATA[["NAME", "LATITUDE", "LONGITUDE"]]
-# print(GLACIER_DATA)
 
 def get_lat_long(name: str) -> tuple:
     # gd = GLACIER_DATA.query(f"NAME == {name}")
@@ -44,12 +39,7 @@ def get_elevation_from_region(lat, long):
         for j in long_range:
             lis.append(make_elevation_request(i, j))
     return lis
-            #NB. Original query string below. It seems impossible to parse and
-#reproduce query strings 100% accurately so the one below is given
-#in case the reproduced version is not "correct".
-# response = requests.get('https://www.freemaptools.com/ajax/elevation-service.php?v=4&lat=-64.00000&lng=-65.00000', headers=headers)
-
-
+    
 if __name__ == "__main__":
     # print(GLACIER_DATA)
     thunder = get_lat_long("THUNDER")
@@ -57,13 +47,3 @@ if __name__ == "__main__":
 
     print(make_elevation_request(thunder[0], thunder[1]))
     print(get_elevation_from_region(thunder[0], thunder[1]))
-    # for col in GLACIER_DATA.columns:
-    #     print(len(col))
-    # elevation.clip(bounds=(thunder[0] - 0.1, thunder[1] - 0.1, thunder[0] + 0.1, thunder[1] + 0.1), output='test.tif')
-
-    # elevation.clean()
-    # elevation_function(get_lat_long("THUNDER"))
-    # print(get_lat_long("NO 4"))
-
-
-    # print(df)
