2e811f057b59e20dbe4cde34e8e9a769210681d1
siddha <siddhakilaru@gmail.com>
2023-02-09 17:18:58 -0500

    added db file. might not stay as its size may increase > 100mb

--- api/sql_convert.py
+++ api/sql_convert.py
@@ -1,5 +1,18 @@
 import sqlite3
+import pandas as pd
 from dbfread import DBF
 
-for record in DBF("data/glims_polygons.dbf"):
+
+
+con = sqlite3.connect("glacier.db")
+cur = con.cursor()
+cur.execute("DROP TABLE IF EXISTS glaciers")
+cur.execute("CREATE TABLE glaciers(glacier_name)")
+
+
+for i, record in enumerate(DBF("data/glims_polygons.dbf")):
     print(record)
+    if i > 1:
+        break
+
+print(pd.read_sql_query("SELECT * FROM glaciers", con))
