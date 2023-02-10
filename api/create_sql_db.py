2bcdbd0c58690ffca979b4fe2d7a0205fb6048de
siddha <siddhakilaru@gmail.com>
2023-02-10 14:10:29 -0500

    finished the sqlite db i think

--- api/create_sql_db.py
+++ api/create_sql_db.py
@@ -2,23 +2,51 @@ import sqlite3
 import pandas as pd
 from dbfread import DBF
 
+"""
+The data used for this script is from this website: https://www.glims.org/.
+You got to make an account to download it and there are glims_polygons.dbf files.
+"""
 
+if __name__ == '__main__':
+    print("NOTE: running the program will create/overwrite glacier.db. press y to continue.")
+    choice = input().lower()
+    if not choice in {"yes", "y", "ye", ""}:
+        quit()
 
-con = sqlite3.connect("glacier.db")
-cur = con.cursor()
-cur.execute("DROP TABLE IF EXISTS glaciers;")
-cur.execute("CREATE TABLE glaciers (glacier_name TEXT, \
-                                    UNIQUE(glacier_name));")
+    con = sqlite3.connect("glacier.db")
+    cur = con.cursor()
+    cur.execute("DROP TABLE IF EXISTS glaciers;")
+    cur.execute("CREATE TABLE glaciers (glacier_name TEXT, \
+                                        source_time TEXT, \
+                                        area REAL, \
+                                        mean_elev INTEGER, \
+                                        min_elev INTEGER, \
+                                        max_elev INTEGER, \
+                                        UNIQUE (glacier_name, source_time));")
 
+    for i, record in enumerate(DBF("data/glims_polygons.dbf", char_decode_errors="ignore")):
+        glacier_name = record["glac_name"].replace("'", "").strip()
+        source_time = record["src_date"]
+        area = record["db_area"]
+        mean_elev = record["mean_elev"]
+        min_elev = record["min_elev"]
+        max_elev = record["max_elev"]
+        
+        if glacier_name == "None":
+            continue
+        print(glacier_name)
 
-for i, record in enumerate(DBF("data/glims_polygons.dbf")):
-    glacier_name = record["glac_name"]
-    if glacier_name == "None":
-        continue
-    
-    print(glacier_name)
-    cur.execute(f"INSERT OR REPLACE INTO glaciers (glacier_name) VALUES('{glacier_name}');")
-    if i > 50:
-        break
+        cur.execute(f"INSERT OR REPLACE INTO glaciers (glacier_name, \
+                                                       source_time, \
+                                                       area, \
+                                                       mean_elev, \
+                                                       min_elev, \
+                                                       max_elev) \
+                                            VALUES('{glacier_name}', \
+                                                   '{source_time}', \
+                                                    {area}, \
+                                                    {min_elev}, \
+                                                    {max_elev}, \
+                                                    {mean_elev});")
 
-print(pd.read_sql_query("SELECT * FROM glaciers", con))
+    print(pd.read_sql_query("SELECT * FROM glaciers", con))
