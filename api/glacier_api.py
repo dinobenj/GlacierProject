46b6ec43fc8d2e58e2e37281df027a5aee40add2
siddhak <siddhakilaru@gmail.com>
2023-02-14 16:35:55 -0500

    progress on api

--- api/glacier_api.py
+++ api/glacier_api.py
@@ -1,14 +1,40 @@
+import sqlite3
 from flask import Flask, jsonify, request
 from flask_restful import Resource, Api
   
 app = Flask(__name__)
 api = Api(app)
 
+con = sqlite3.connect("glacier.db")
+cur = con.cursor()
+
 class Test(Resource):
+    def __init__(self):
+        Test.args = []
     def get(self):
-        return jsonify({'message': 'hello world'})
+        args = request.args
+        print(len(args))
+        print(args.keys())
+        return jsonify({"message": f"hello world {args}"})
+    
 
-api.add_resource(Test, '/test') 
+class Glacier(Resource):
+    def __init__(self):
+        self.name_arg = "name"
+        self.list_all_arg = "list_all"
+    def get(self):
+        args = request.args
+        if len(args) > 1:
+            return "Too many args", 400
+        if not (self.name_arg in args.keys() or self.list_all_arg in args.keys()):
+            return "Invalid arg: require 'name' or 'list_all'", 400
+        else:
+            return jsonify({"message": f"hello world {args}"})
 
-if __name__ == '__main__':
+
+
+api.add_resource(Test, "/test") 
+api.add_resource(Glacier, "/glacier")
+
+if __name__ == "__main__":
     app.run(port=80, debug = True)
