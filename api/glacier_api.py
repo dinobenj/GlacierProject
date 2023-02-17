85ca418cebf071b882ac311f20e43db71c19a703
siddha <siddhakilaru@gmail.com>
2023-02-17 15:02:34 -0500

    listing all the glaciers works

--- api/glacier_api.py
+++ api/glacier_api.py
@@ -9,13 +9,6 @@ api = Api(app)
 con = sqlite3.connect("glacier.db", check_same_thread=False)
 cur = con.cursor()
 
-print(pd.read_sql_query("SELECT * FROM glaciers", con))
-
-cur.execute("select * from glaciers")
-results = cur.fetchall()
-print(len(results))
-
-
 class Glacier(Resource):
     def __init__(self):
         self.name_arg = "name"
@@ -25,8 +18,10 @@ class Glacier(Resource):
         if len(args) > 1:
             return "Too many args", 400
         elif self.list_all_arg in args.keys(): # dumps all the glacier names
-            print(cur.execute("SELECT glacier_name FROM glaciers").fetchall())
-            return jsonify({"listing all": f"hello world {args[self.list_all_arg]}"})
+            if args[self.list_all_arg] == "true":
+                return jsonify({"glaciers": cur.execute("SELECT glacier_name FROM glaciers").fetchall()})
+            else:
+                return jsonify({"glaciers": ""})
         elif self.name_arg in args.keys():
             return jsonify({"name": f"hello world {args[self.name_arg]}"})
         else:
