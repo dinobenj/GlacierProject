adeb6b7aa59dbac814c47b6fe10508b2013d5e43
williamlin6803 <williamlin6803@gmail.com>
2023-02-17 23:22:52 -0500

    Playing around with Main Page

--- react-app/src/components/ MemberIntroduction.tsx
+++ react-app/src/components/ MemberIntroduction.tsx
@@ -1,43 +1,53 @@
-import {Col, Row, Typography} from "antd";
+import { Col, Row, Typography } from "antd";
 import React from "react";
+import "./MemberIntroduction.css"; // Import custom CSS file
 
-const {Paragraph} = Typography;
+const { Paragraph } = Typography;
 
 const MemberIntroduction: React.FC = () => {
-    return (
-        <>
-            <Row>
-                <Col>
-                    <Paragraph strong style={{
-                        color: "white",
-                        fontSize: "20px"
-                    }}>
-                        Project Lead: Ben Dennison
-                    </Paragraph>
-                </Col>
-            </Row>
-            <Row>
-                <Col>
-                    <Paragraph strong style={{
-                        color: "white",
-                        fontSize: "20px"
-                    }}>
-                        Frontend: Ji Wang, Russell Harter, Kaylin Rackley, William Lin
-                    </Paragraph>
-                </Col>
-            </Row>
-            <Row>
-                <Col>
-                    <Paragraph strong style={{
-                        color: "white",
-                        fontSize: "20px"
-                    }}>
-                        Backend: Russell Harter, Ryan Karch, Smayan Nandakumar, Sarah Nicolai
-                    </Paragraph>
-                </Col>
-            </Row>
-        </>
-    );
-}
+  return (
+    <>
+      <Row style={{ marginBottom: "20px" }}>
+        <Col>
+          <Paragraph
+            strong
+            className="member-intro-text"
+          >
+            Project Lead: Ben Dennison
+          </Paragraph>
+        </Col>
+      </Row>
+      <Row style={{ marginBottom: "20px" }}>
+        <Col>
+          <Paragraph
+            strong
+            className="member-intro-text"
+          >
+            Frontend: Ji Wang, Russell Harter, Kaylin Rackley, William Lin
+          </Paragraph>
+        </Col>
+      </Row>
+      <Row style={{ marginBottom: "20px" }}>
+        <Col>
+          <Paragraph
+            strong
+            className="member-intro-text"
+          >
+            Backend: Russell Harter, Ryan Karch, Smayan Nandakumar, Sarah Nicolai
+          </Paragraph>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <Paragraph
+            className="member-intro-text"
+          >
+            All students are from Rensselaer Polytechnic Institute.
+          </Paragraph>
+        </Col>
+      </Row>
+    </>
+  );
+};
 
 export default MemberIntroduction;
