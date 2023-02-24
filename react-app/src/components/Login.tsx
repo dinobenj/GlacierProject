8165b705978b2f24338bbb12a37e699fc03c8917
ow44 <owenwitkos@gmail.com>
2023-02-24 17:08:11 -0500

    Update Login.tsx

--- react-app/src/components/Login.tsx
+++ react-app/src/components/Login.tsx
@@ -1,2 +1,12 @@
 import React from "react";
 import {Button, Col, Row, Typography} from "antd";
+
+const Login: React.FC = () => {
+    return (
+        <header>
+            TEST
+        </header>
+    )
+}
+
+export default Login;
