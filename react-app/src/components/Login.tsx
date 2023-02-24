3cf621a7d9e5d1e28dfcf5633f7a7ce7d2fa86f9
ow44 <owenwitkos@gmail.com>
2023-02-24 17:01:50 -0500

    login button

---
+++ react-app/src/components/Login.tsx
@@ -0,0 +1,2 @@
+import React from "react";
+import {Button, Col, Row, Typography} from "antd";
--- react-app/src/index.tsx
+++ react-app/src/index.tsx
@@ -6,10 +6,12 @@ import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import Home from './components/Home';
 import Map from "./components/Map";
 import About from "./components/About";
+import Login from "./components/Login";
 import SiteHeader from './components/SiteHeader';
 import SiteFooter from './components/SiteFooter';
 import 'antd/dist/antd.min.css';
 import {Layout} from 'antd';
+
 
 const {Content} = Layout;
 
@@ -25,6 +27,10 @@ const router = createBrowserRouter([
     {
         path: "/about",
         element: <About/>
+    },
+    {
+        path: "/login",
+        element: <Login/>
     }
 ]);
 
