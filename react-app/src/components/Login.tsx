f8589b9b9d2cdabaea2e35b33b62244f893f2e49
williamlin6803 <williamlin6803@gmail.com>
2023-03-03 21:35:08 -0500

    Login

--- react-app/src/components/Login.tsx
+++ react-app/src/components/Login.tsx
@@ -1,7 +1,7 @@
 import React from "react";
-import { Button, Col, Row, Typography } from "antd";
-const css = require("./MemberIntroduction.css");
-//import { useForm } from "useForm";
+import { Button, Col, Form, Input, Row, Typography } from "antd";
+import MemberIntroduction from "./MemberIntroduction";
+import { useForm } from "../useForm";
 
 const { Title, Paragraph } = Typography;
 
@@ -19,7 +19,7 @@ const Login: React.FC = () => {
   };
 
   // getting the event handlers from our custom hook
-  //const { onChange, onSubmit, values } = useForm(loginUserCallback, initialState);
+  const { onChange, onSubmit, values } = useForm(loginUserCallback, initialState);
 
   // a submit function that will execute upon form submission
   async function loginUserCallback() {
@@ -27,9 +27,40 @@ const Login: React.FC = () => {
   }
 
   return (
-    <header>
-      TEST
-    </header>
+    <Row justify="center">
+      <Col xs={24} sm={20} md={16} lg={12} xl={8}>
+        <Title level={2}>Login</Title>
+        <Form onFinish={onSubmit} layout="vertical">
+          <Form.Item
+            label="Email"
+            name="email"
+            rules={[{ required: true, message: "Please input your email!" }]}
+          >
+            <Input value={values.email} onChange={onChange} type="email" />
+          </Form.Item>
+
+          <Form.Item
+            label="Password"
+            name="password"
+            rules={[{ required: true, message: "Please input your password!" }]}
+          >
+            <Input.Password value={values.password} onChange={onChange} />
+          </Form.Item>
+
+          <Form.Item>
+            <Button type="primary" htmlType="submit">
+              Login
+            </Button>
+          </Form.Item>
+        </Form>
+
+        <Paragraph>
+          {signupText} <a href="#">{signupButton}</a>
+        </Paragraph>
+
+        <MemberIntroduction title={teamIntroductionTitle} text={teamIntroductionText} />
+      </Col>
+    </Row>
   );
 };
 
