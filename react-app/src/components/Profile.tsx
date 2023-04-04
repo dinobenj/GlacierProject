978656cf1a78c78ed64021092723446cef6e0e26
ow44 <owenwitkos@gmail.com>
2023-04-04 16:45:54 -0400

    change info forms

--- react-app/src/components/Profile.tsx
+++ react-app/src/components/Profile.tsx
@@ -11,6 +11,10 @@ interface UserData {
 
 const Profile: React.FC = () => {
 
+    const initialStateEmail = {
+        email: "",
+        confirmEmail: "",
+    }
 
 
     return (
@@ -25,7 +29,7 @@ const Profile: React.FC = () => {
                         <Title level={1} style={{ color: "white" }}>{"Profile:"}</Title>
                         <Title level={4} style={{ color: "white" }}>{"Username:"}</Title>
                         <Paragraph strong style={{ color: "white", fontSize: "20px" }}>
-                            {"<name>"}
+                            {" "}
                         </Paragraph>
                     </Col>
                 </Row>
@@ -33,19 +37,72 @@ const Profile: React.FC = () => {
                     <Col>
                         <Title level={4} style={{ color: "white" }}>{"Email:"}</Title>
                         <Paragraph strong style={{ color: "white", fontSize: "20px" }}>
-                            {"<email>"}
+                            {" "}
                         </Paragraph>
                     </Col>
                 </Row>
                 <Row>
                     <Col>
                         <Title level={2} style={{ color: "white" }}>{"Change Email:"}</Title>
-                        
+                        <form>
+                            <div style={{
+                                display: "flex",
+                                flexDirection: "column",
+                                justifyContent: "center",
+                                alignItems: "start"
+                            }}>
+                                <input
+                                    name='newEmail'
+                                    id='newEmail'
+                                    type='newEmail'
+                                    placeholder='New Email'
+                                    //onChange={}
+                                    required
+                                    />
+                                <input
+                                    name='confirmEmail'
+                                    id='confirmEmail'
+                                    type='confrimEmail'
+                                    placeholder='Confirm Email'
+                                    //onChange={}
+                                    required
+                                    />
+                                <br></br>
+                                <button type='submit'>Change Email</button>
+                            </div>
+                        </form>
                     </Col>
                 </Row>
                 <Row>
                     <Col>
                         <Title level={2} style={{ color: "white" }}>{"Change Password:"}</Title>
+                        <form>
+                            <div style={{
+                                display: "flex",
+                                flexDirection: "column",
+                                justifyContent: "normal",
+                                alignItems: "start"
+                            }}>
+                                <input
+                                    name='newPassword'
+                                    id='newPassword'
+                                    type='newPassword'
+                                    placeholder='New Password'
+                                    //onChange={}
+                                    required
+                                    />
+                                <input
+                                    name='confirmPassword'
+                                    id='confirmPassword'
+                                    type='confrimPassword'
+                                    placeholder='Confirm Password   '
+                                    //onChange={}
+                                    required
+                                    />
+                                <br></br>
+                                <button type='submit'>{"Change Password"}</button>
+                            </div>
+                        </form>
                     </Col>
                 </Row>
             </Col>
