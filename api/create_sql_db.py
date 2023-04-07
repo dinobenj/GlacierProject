0bbaaa1cc82780d97b35d0e00f650c8fc8558d2f
siddha <siddhakilaru@gmail.com>
2023-04-07 09:51:40 -0400

    final adjustments

--- api/create_sql_db.py
+++ api/create_sql_db.py
@@ -3,38 +3,43 @@ import pandas as pd
 from dbfread import DBF
 
 """
-The data used for this script is from this website: https://www.glims.org/.
-You got to make an account to download it and there are glims_polygons.dbf files.
+The data used for this script is from this website: https://www.glims.org/. You
+got to make an account to download it and there are glims_polygons.dbf files
+for different years. You can run this script several times for different glims
+dbf files and add to the same database file. 
 """
 
 
 if __name__ == "__main__":
-    print("NOTE: running the program will create/overwrite glacier.db. press y to continue.")
+
+    print("Enter the name of the database file to create or modify:")
+    db_filename = input().lower()
+    
+    print("Enter the directory of the glims polygon dbf file:")
+    glims_file = input().lower()
+    
+    print(f"NOTE: running the program will create/modify {db_filename}.db using the contents from {glims_file}. press y to continue.")
     choice = input().lower()
     if not choice in {"yes", "y", "ye", ""}:
         quit()
-
-    # print("Enter the name of the database file to create or modify")
-    # db_filename = input().lower()
     
     # The data base file will be created if it does not already exist.
-    con = sqlite3.connect("glacier.db")
+    con = sqlite3.connect(f"{db_filename}.db")
     cur = con.cursor()
     
     # If the glacier table exists, it will be dropped.
-    cur.execute("DROP TABLE IF EXISTS glaciers;")
-    cur.execute("CREATE TABLE glaciers (glacier_name TEXT, \
-                                        source_time TEXT, \
-                                        area REAL, \
-                                        mean_elev INTEGER, \
-                                        min_elev INTEGER, \
-                                        max_elev INTEGER, \
-                                        UNIQUE (glacier_name, source_time));")
+    cur.execute("CREATE TABLE IF NOT EXISTS glaciers (glacier_name TEXT, \
+                                                      source_time TEXT, \
+                                                      area REAL, \
+                                                      mean_elev INTEGER, \
+                                                      min_elev INTEGER, \
+                                                      max_elev INTEGER, \
+                                                      UNIQUE (glacier_name, source_time));")
     con.commit()
 
     # If you want to read from a different glims data base, the file name below must be changed.
-    for i, record in enumerate(DBF("data/glims_polygons.dbf", char_decode_errors="ignore")):
-        
+    for i, record in enumerate(DBF(glims_file, char_decode_errors="ignore")):
+
         # Here we iterate through all the records in the glim file.
         # Only the parameters below are stored in the sql db.
         glacier_name = record["glac_name"].replace("'", "").strip()
