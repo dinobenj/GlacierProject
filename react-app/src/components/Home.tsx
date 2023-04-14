adfdb3dc731838f34b003360d5c7d9bb08213cb0
williamlin6803 <williamlin6803@gmail.com>
2023-04-14 17:03:27 -0400

    Edited styles in Home

--- react-app/src/components/Home.tsx
+++ react-app/src/components/Home.tsx
@@ -1,63 +1,51 @@
 import React from "react";
-import {Button, Col, Row, Typography} from "antd";
+import { Button, Col, Row, Typography } from "antd";
+import "./Home.css";
 
-const {Title, Paragraph, Text} = Typography;
+const { Title, Paragraph, Text } = Typography;
 
 const Home: React.FC = () => {
-    const titleText = "Do you know?";
-    const glacierIntroductionText = "Glaciers are masses of snow that has been compressed into giant sheets of ice. Most glaciers were formed during the last ice age.";
-    const appIntroductionText = "We built an app to let you see glaciers all over the world, and their detailed data.";
-    const welcomeText = "Welcome to the world of ice!";
-    const buttonText = "GO TO MAP";
+  const titleText = "Do you know?";
+  const glacierIntroductionText =
+    "Glaciers are masses of snow that has been compressed into giant sheets of ice. Most glaciers were formed during the last ice age.";
+  const appIntroductionText =
+    "We built an app to let you see glaciers all over the world, and their detailed data.";
+  const welcomeText = "Welcome to the world of ice!";
+  const buttonText = "GO TO MAP";
 
-
-    return (
-        <Row justify="center" align="middle" style={{height: "100vh"}}>
-            <Col span={12} style={{
-                padding: "20px",
-                borderRadius: "20px",
-                backgroundColor: "rgba(128, 128, 128, 0.3)"
-            }}>
-                <Row justify="space-around" align="middle">
-                    <Col>
-                        <Title style={{color: "white"}}>{titleText}</Title>
-                    </Col>
-                </Row>
-                <Row justify="space-around" align="middle">
-                    <Col span={24}>
-                        <Paragraph strong style={{
-                            color: "white",
-                            fontSize: "20px"
-                        }}>
-                            {glacierIntroductionText}
-                        </Paragraph>
-                        <Paragraph strong style={{
-                            color: "white",
-                            fontSize: "20px"
-                        }}>
-                            {appIntroductionText}
-                        </Paragraph>
-                        <Paragraph strong style={{
-                            color: "white",
-                            fontSize: "20px",
-                            textAlign: "center"
-                        }}>
-                            {welcomeText.toUpperCase()}
-                        </Paragraph>
-                    </Col>
-                </Row>
-                <Row justify="space-around" align="middle">
-                    <Col>
-                        <Button ghost size="large" shape="round" href="/map">
-                            <Text strong style={{color: "white"}}>
-                                {buttonText}
-                            </Text>
-                        </Button>
-                    </Col>
-                </Row>
-            </Col>
+  return (
+    <Row justify="center" align="middle" className="home-container">
+      <Col span={12} className="home-card">
+        <Row justify="space-around" align="middle">
+          <Col>
+            <Title className="home-title">{titleText}</Title>
+          </Col>
         </Row>
-    );
-}
+        <Row justify="space-around" align="middle">
+          <Col span={24}>
+            <Paragraph strong className="home-paragraph">
+              {glacierIntroductionText}
+            </Paragraph>
+            <Paragraph strong className="home-paragraph">
+              {appIntroductionText}
+            </Paragraph>
+            <Paragraph strong className="home-welcome-text">
+              {welcomeText.toUpperCase()}
+            </Paragraph>
+          </Col>
+        </Row>
+        <Row justify="space-around" align="middle">
+          <Col>
+            <Button ghost size="large" shape="round" to="/map">
+              <Text strong className="home-button-text">
+                {buttonText}
+              </Text>
+            </Button>
+          </Col>
+        </Row>
+      </Col>
+    </Row>
+  );
+};
 
 export default Home;
