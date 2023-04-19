05aa9356c6ca3c5b23e19534c5e0b2b6957dc691
siddha <siddhakilaru@gmail.com>
2023-04-18 22:21:08 -0400

    progress on server

--- api/server/main.c
+++ api/server/main.c
@@ -10,6 +10,22 @@
 
 int main(int argc, char** argv) {
 
+    struct sockaddr_in sa;
+    memset(&sa, 0, sizeof(sa));
+    int socket_fd = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (socket_fd == -1) {
+        fprintf(stderr, "Error: Cannot create socket.\n");
+        return EXIT_FAILURE;
+    }
+    #ifdef PRINT_FD
+    printf("socket fd: %d\n", socket_fd);
+    #endif
+
+    sa.sin_family = AF_INET;
+    sa.sin_port = htons(8080);
+    sa.sin_addr.s_addr = htonl(INADDR_ANY);
+
+
     printf("hello there\n");
 
     return EXIT_SUCCESS;
