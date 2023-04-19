2ffcba4de028c44d97bff2cdeada450114d67520
siddha <siddhakilaru@gmail.com>
2023-04-18 22:03:48 -0400

    main made

---
+++ api/server/main.c
@@ -0,0 +1,16 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+
+int main(int argc, char** argv) {
+
+    printf("hello there\n");
+
+    return EXIT_SUCCESS;
+}
