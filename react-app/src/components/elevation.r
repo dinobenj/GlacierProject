31bfef731fa4d55f12f81dcc5eb5f024390e70e2
williamlin6803 <williamlin6803@gmail.com>
2023-04-21 22:59:47 -0400

    elevation.r update

--- react-app/src/components/elevation.r
+++ react-app/src/components/elevation.r
@@ -2,20 +2,20 @@
 library(raster)
 
 # Set working directory
-setwd("path/to/working/directory")
+setwd("C:/Users/William Lin/Desktop/GlacierProject/react-app/src/components")
 
 # Load elevation data
-elevation_data <- raster("path/to/elevation/file.tif")
+elevation_data <- raster("elevation/file.tif")
 
-# Load glacier data
-glacier_data <- shapefile("path/to/glacier/shapefile.shp")
+# Load glacier data from CSV file
+glacier_data <- read.csv("glacier.csv")
 
 # Extract elevation values for glacier locations
-glacier_elevation <- extract(elevation_data, glacier_data)
+glacier_elevation <- extract(elevation_data, as.matrix(glacier_data[, c("longitude", "latitude")]), method="bilinear")
 
 # Create a color palette
 colors <- colorRampPalette(c("white", "blue"))
 
 # Plot the elevation map with glacier locations
 plot(elevation_data, col=colors(100), main="Elevation Map for Glaciers")
-plot(glacier_data, add=TRUE, pch=20, col="red")
+points(glacier_data$longitude, glacier_data$latitude, pch=20, col="red", cex=0.7)
